9267ec9e856f408a6c0ae034e95afb31794f6956
RICCOM <ericomun@gmail.comgit>
2024-04-28 09:57:47 +0300

    Adds Dynamic Components with props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  function Displaybio(props){
+      if (!props.bio){
+        return null;
+      }else{
+        return <p>{props.bio}</p>
+      }
+    }
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <Displaybio bio={props.bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
     </div>
   );
 }
